9c532fd22002d1f5c71176c77f029cc95f0ab253
Thiago Ferreira <thiagodnf@gmail.com>
2023-02-25 20:50:07 -0500

    m

--- dist/index.js
+++ dist/index.js
@@ -43607,8 +43607,23 @@ var CardBody = function () {
 
     var plugin = {
         type: "lang",
-        regex: /\[!CardBody\]((\n|\r|.)*?)\[CardBody\]/g,
-        replace: "<div class='card-body'>$1</div>"
+        // regex: /\[!CardBody\]((\n|\r|.)*?)\[CardBody\]/g,
+        // replace: "<div class='card-body'>$1</div>",
+        filter: function (text, converter) {
+
+            var regex = new RegExp(/\[!CardBody\]((\n|\r|.)*?)\[CardBody\]/, "g");
+
+            text = text.trim().replace(regex, function (match, content) {
+
+                content = converter.makeHtml(content.trim());
+
+                //content = content.replace(/^<p>(.*)<\/p>$/, "$1");
+
+                return `<div class='card-body'>${content}</div>`;
+            });
+
+            return text;
+        }
     };
 
     return [plugin];
--- test/parser/Parser.test.js
+++ test/parser/Parser.test.js
@@ -3,7 +3,11 @@ import parser from "../../src/parser/Parser.js";
 test("throws invalid number", () => {
 
     const md = `
-        [!CardHeader]**Header 1**[CardHeader]
+        [!CardBody]
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+
+[Link text Here](https://link-url-here.org)
+        [CardBody]
     `;
 
     const html = parser(md, "");
