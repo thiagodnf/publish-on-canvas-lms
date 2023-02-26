c7fd4223c6607ea551cfa271732a3cb4ab695342
Thiago Ferreira <thiagodnf@gmail.com>
2023-02-25 20:09:34 -0500

    p

--- dist/index.js
+++ dist/index.js
@@ -43621,8 +43621,21 @@ var CardHeader = function () {
 
     var plugin = {
         type: "lang",
-        regex: /\[!CardHeader\]((\n|\r|.)*?)\[CardHeader\]/g,
-        replace: "<div class='card-header'>$1</div>"
+        // regex: /\[!CardHeader\]((\n|\r|.)*?)\[CardHeader\]/g,
+        // replace: "<div class='card-header'>$1</div>"
+        filter: function (text, converter, options) {
+
+            var regex = /\[!CardHeader\]((\n|\r|.)*?)\[CardHeader\]/g;
+
+            text = text.replace(regex, function (match, content) {
+
+                content = converter.makeHtml(content);
+
+                return `<div class='card'>${content}</div>`;
+            });
+
+            return text;
+        }
     };
 
     return [plugin];
