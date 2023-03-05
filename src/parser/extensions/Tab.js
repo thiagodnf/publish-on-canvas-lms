9e4c1620b4f803c13c821851aaeb0c7e753912f5
Thiago Ferreira <thiagodnf@gmail.com>
2023-03-05 17:43:06 -0500

    a

--- src/parser/extensions/Tab.js
+++ src/parser/extensions/Tab.js
@@ -4,11 +4,11 @@ var Tab = function () {
         type: "lang",
         filter: function (text, converter, options) {
 
-            var regex = new RegExp("\\[!tab\\](.*)\\[tab\\]", "s");
+            var regex = new RegExp(/\[tab\](.*)\[\/tab\]/, "s");
 
             text = text.replace(regex, function (match1, items) {
 
-                var itemRegex = new RegExp("((?:\\[!tabitem\\]\\((.+)\\)((\\n|\\r|.)*?)\\[tabitem\\]))", "gm");
+                var itemRegex = new RegExp("((?:\\[tabitem\\]\\((.+)\\)((\\n|\\r|.)*?)\\[\/tabitem]))", "gm");
 
                 let navItems = [];
                 let tabPane = [];
